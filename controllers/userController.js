0d0c006cc008193a52afd231f82a603cd7b83f21
Daniel Vidaillet Rojas <dvidaillet90@gmail.com>
2024-09-20 14:40:25 -0500

    add uuid to project

--- controllers/userController.js
+++ controllers/userController.js
@@ -4,11 +4,21 @@ import User from "../models/User.js";
 // Crear un nuevo usuario
 export const createUser = async (req, res) => {
   try {
-    const user = new User(req.body);
+    const user = new User(req.body); // El campo `id` se generará automáticamente
     await user.save();
     res.status(201).json(user);
   } catch (error) {
     res.status(400).json({ message: error.message });
+  }
+};
+
+// Obtener todos los usuarios
+export const getUsers = async (req, res) => {
+  try {
+    const users = await User.find(); // Esto obtiene todos los usuarios
+    res.status(200).json(users);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
   }
 };
 
@@ -21,5 +31,15 @@ export const updateUser = async (req, res) => {
     res.status(200).json(user);
   } catch (error) {
     res.status(400).json({ message: error.message });
+  }
+};
+
+// Eliminar usuario por ID
+export const deleteUser = async (req, res) => {
+  try {
+    await User.findByIdAndDelete(req.params.id);
+    res.status(204).json({ message: "User deleted" });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
   }
 };
--- models/User.js
+++ models/User.js
@@ -1,19 +1,17 @@
-// models/User.js
-import mongoose from "mongoose";
+import mongoose from 'mongoose';
+import { v4 as uuidv4 } from 'uuid'; // Importa la función para generar UUIDs
 
-const userSchema = new mongoose.Schema(
-  {
-    firstName: { type: String, required: true },
-    lastName: { type: String, required: true },
-    dni: { type: String, required: true, unique: true },
-    address: { type: String, required: true },
-    phone: { type: String, required: true },
-    email: { type: String, required: true, unique: true },
-    role: { type: String, required: true },
-    isActive: { type: Boolean, default: true },
-    avatar: { type: String, default: null }, // Campo para la imagen del avatar
-  },
-  { timestamps: true }
-);
+const userSchema = new mongoose.Schema({
+  id: { type: String, default: uuidv4 }, // Genera un UUID como ID por defecto
+  firstName: { type: String, required: true },
+  lastName: { type: String, required: true },
+  dni: { type: String, required: true, unique: true },
+  address: { type: String, required: true },
+  phone: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+  role: { type: String, required: true },
+  isActive: { type: Boolean, default: true },
+  avatar: { type: String, default: null }
+}, { timestamps: true });
 
-export default mongoose.model("User", userSchema);
+export default mongoose.model('User', userSchema);
