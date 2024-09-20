38fe9a6012e51e3ae33560341c096e52236575c6
Daniel Vidaillet Rojas <dvidaillet90@gmail.com>
2024-09-20 15:14:18 -0500

    add get user by id to user controller

--- controllers/userController.js
+++ controllers/userController.js
@@ -43,3 +43,16 @@ export const deleteUser = async (req, res) => {
     res.status(500).json({ message: error.message });
   }
 };
+
+// Obtener un usuario por ID
+export const getUserById = async (req, res) => {
+  try {
+    const user = await User.findById(req.params.id);
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+    res.status(200).json(user);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -5,12 +5,14 @@ import {
   getUsers,
   updateUser,
   deleteUser,
+  getUserById
 } from "../controllers/userController.js";
 
 const router = express.Router();
 
 router.post("/users", createUser);
 router.get("/users", getUsers);
+router.get('/:id', getUserById); 
 router.put("/users/:id", updateUser);
 router.delete("/users/:id", deleteUser);
 
