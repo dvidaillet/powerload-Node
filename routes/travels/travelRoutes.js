f9342fd894de1f8e16a1aea953ca701c614bceae
Daniel Vidaillet Rojas <dvidaillet90@gmail.com>
2024-09-28 14:26:16 -0500

    fix travel cotroller

--- routes/travels/travelRoutes.js
+++ routes/travels/travelRoutes.js
@@ -1,18 +1,12 @@
 import express from "express";
-import {
-  createLoad,
-  getAllLoads,
-  getLoadById,
-  updateLoad,
-  deleteLoad,
-} from "../../controllers/loads/loadController.js";
+import { createTravel, deleteTravel, getAllTravels, getTravelById, updateTravel } from "../../controllers/travels/travelController.js";
 
 const travelRouter = express.Router();
 
-travelRouter.post("/travels", createLoad);
-travelRouter.get("/travels", getAllLoads);
-travelRouter.get("/travels/:id", getLoadById);
-travelRouter.put("/travels/:id", updateLoad);
-travelRouter.delete("/travels/:id", deleteLoad);
+travelRouter.post("/travels", createTravel);
+travelRouter.get("/travels", getAllTravels);
+travelRouter.get("/travels/:id", getTravelById);
+travelRouter.put("/travels/:id", updateTravel);
+travelRouter.delete("/travels/:id", deleteTravel);
 
 export default travelRouter;
